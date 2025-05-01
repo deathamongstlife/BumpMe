ce92c3dfb3046591419f00e0f320237a8a8b24b3
deathamongstlife <r79767525@gmail.com>
2025-05-01 01:02:06 -0400

    Update BotDM.js

--- src/events/guildCreate/BotDM.js
+++ src/events/guildCreate/BotDM.js
@@ -6,7 +6,7 @@ module.exports = async (client, guild) => {
         const guildOwner = await guild.fetchOwner();
         
         // Define the embed message
-        const dotEmoji2 = '<:Arrow:1281677131004252191>';
+        const dotEmoji2 = '<:Arrow:1307458715473154048>';
         const embed = new EmbedBuilder()
         	.setTitle("Thanks for adding BumpMe!")
             .addFields(
@@ -15,16 +15,12 @@ module.exports = async (client, guild) => {
                     value: `${dotEmoji2} [**Click here**](https://discord.com/oauth2/authorize?client_id=1265343375054340160) to invite BumpMe!`
                 },
                 {
-                    name: "**Login to Dashboard** (`Required`)",
-                    value: `${dotEmoji2} head over to https://www.bumping.ltd to login to the dashboard.`
+                    name: "**Setup server** (`Required`)",
+                    value: `${dotEmoji2} run /setup channel, /setup invite, and /setup description`
                 },
                 {
-                    name: "**Edit your server** (`Required`)",
-                    value: `${dotEmoji2} Setup your server with our brand new server edit pages.`
-                },
-                {
-                    name: "**Save your settings** (`Required`)",
-                    value: `${dotEmoji2} Save your server settings, bump channel/invite channel and server description - save them and get to bumping!`
+                    name: "**Bump your server** (`Required`)",
+                    value: `${dotEmoji2} run /bump to grow!`
                 }
             )
         	.setImage("https://media.discordapp.net/attachments/1265026826473046038/1281679675168395347/Bump2You_1.png?ex=66dc98b7&is=66db4737&hm=6240b6fefd2343a77990b1b14b12686f55da7e40c6f956dec1aa1023eef05154&=&format=webp&quality=lossless&width=1295&height=78")
@@ -32,11 +28,11 @@ module.exports = async (client, guild) => {
 
         // Send the embed message to the guild owner's DM channel
         await guildOwner.send({
-            content: "<:link:1241836480779784202> https://discord.gg/f75Qytv9cT",
+            content: "<:link:1367364911302447236> https://discord.gg/A2ANr4tnTH",
             embeds: [embed], 
         });
         
-        return guildOwner.send("**TRY OUT DISCORD PROMOTIONS FOR MORE GROWTH!** https://discord.gg/RJh763aF")
+        return guildOwner.send("**TRY OUT DISCORD PROMOTIONS FOR MORE GROWTH!** https://discord.gg/vudnye3jSC\n\n**Also join Supernatural Promotions!** https://discord.gg/w2PsZqaVMz")
     } catch (error) {
         console.log('Error sending message:', error);
     }
